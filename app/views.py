fb9c561e072dfae77a8724a002b225dfbc3f0836
Saikumar <saikumar595925@gmail.com>
2023-06-01 13:46:13 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def virat(request):
+    return render(request,'virat.html')
+
+def anushka(request):
+    return render(request,'anushka.html')
